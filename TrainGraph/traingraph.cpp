a71b992b0840afc30f60fa873ecd8be3afb2fee6
NoremacSkich <noremacskich@gmail.com>
2014-04-14 14:27:27 -0500

    Program Outline Created
    Created:
    1	traingraph.cpp
    2		int main(void)
    3		void getStations(string filePath, STATIONS stations[])
    4		void getTrains(string filePath, TRAINS trainSched[])
    5		void showMainMenu(void)
    6		void showStations(STATIONS stations[])
    7		void showTrainSched(STATIONS stations[])
    8		void myPause(void)
    9		void clearScreen(void)
    
    Reasons:
    1	This is the primary program. This file is going to be based off of the
    	Binary Records/program.cpp file.
    2	Added the main menu driver.
    3-4	We need a way to get the information from the file into an usable array
    5-7	These all display output
    8	This will pause the console to allow the user to see the output
    9	This will clear the screen.

---
+++ TrainGraph/traingraph.cpp
@@ -0,0 +1,285 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <stdlib.h>
+//===============================================================
+//   PROGRAM:  Assignment #3
+//   AUTHOR :  Cameron Hicks
+//   DUE    :  4/30/2014 9:00 AM
+//   DESCRIPTION:
+//			This program will simulate a train terminal, by 
+//			using a graph data structure.  It will need to
+//			find the shortest path between two points, and 
+//			optionally, the shortest time between two points.
+//===============================================================
+using namespace std;
+
+// GLOBAL CONSTANT ====================================
+
+// This is where the stations.dat file is located
+const string stationsFile = "stations.dat";
+
+// this is where the trains.dat file is located
+const string trainsFile = "trains.dat";
+
+
+
+// GLOBAL TYPE DEF  =====================================
+
+/**@struct STATION
+ *	^	This holds all the information related to a station.
+ *
+ * @param Number | int
+ *	^	This is the number of the train station.
+ *	N	This will be in the range of 0 to 99
+ *
+ * @param Name | String
+ *	^	This is the name of the train station.
+ *	N	This will have a max length of 25 characters
+ *
+ * @author NoremacSkich | 2014/4/14
+ * 
+ */
+
+struct STATIONS{
+	int Number;
+	string Name;
+};
+
+/**@struct TRAINS
+ *	^	This holds all the information related to a train.
+ *	N	This program assumes that no train will run from one day to the next.
+ *
+ * @param Departure_Time | int
+ *	^	This is when the train leaves the station according to the number
+ *		of minutes into the day.
+ *	N	There are 1440 minutes in a day.
+ *
+ * @param Arrival_Time | int
+ *	^	This is when the train arrives at the station according to the number
+ *		of minutes into the day.
+ *	N	There are 1440 minutes in a day.
+ *	N	This number should be greater than the departure time.
+ *
+ * @param Departure_Station | int
+ *	^	This is the station were the train will depart from.
+ *	N	There are at most 100 stations.
+ *
+ * @param Arrival_Station | int
+ *	^	The train will arrive at this station.
+ *	N	There are at most 100 stations.
+ *
+ * @author NoremacSkich | 2014/4/14
+ * 
+ */
+
+struct TRAINS{
+	int Departure_Station;
+	int Arrival_Station;
+	int Departure_Time;
+	int Arrival_Time;
+};
+
+// ================= PROTOTYPES FOR STATIONS ========================
+/**@fun getStations(string filePath, STATIONS stations[])
+ *	^	This will open up the inputed file, and return an array of stations.
+ *	N	This function assumes that spaces are being used to distinguish the 
+ *		station number from the station name.
+ *
+ * @param filePath | String
+ *	^	This is the path that leads to a file with the station info.
+ *
+ * @param stations[] | STATIONS
+ *	^	This is the array that the station data will be returned in.
+ *
+ * @author NoremacSkich | 2014/4/14
+ *
+ * @return 
+ *	^	See the stations[] parameter.
+ */
+void getStations(string filePath, STATIONS stations[]);
+
+// ================= PROTOTYPES FOR TRAINS ========================
+/**@fun getTrains(string filePath, TRAINS trainSched[])
+ *	^	This will open up the inputed file, and return an array of the train
+ *		schedule.
+ *	N	This function assumes that spaces are being used to distinguish the 
+ *		arrival and departure time and station.
+ *
+ * @param filePath | String
+ *	^	This is the path that leads to a file with the train schedule.
+ *
+ * @param trainSched[] | STATIONS
+ *	^	This is the array that the train schedule data will be returned in.
+ *
+ * @author NoremacSkich | 2014/4/14
+ *
+ * @return 
+ *	^	See the trainSched[] parameter.
+ */
+void getTrains(string filePath, TRAINS trainSched[]);
+
+// ================= PROTOTYPES FOR THE OUTPUT ========================
+/**@fun showMainMenu(void)
+ *	^	This will print out the main menu.
+ *
+ * @author NoremacSkich | 2014/4/14
+ *
+ */
+ 
+void showMainMenu(void);
+
+/**@fun showStations(STATIONS stations[])
+ *	^	This will print out the stations and their ID to the consumer.
+ * 
+ * @param stations[] | STATIONS
+ *	^	This array should hold the station name and their associated station 
+ *		ID.
+ *
+ * @author NoremacSkich | 2014/4/14
+ *
+ */
+void showStations(STATIONS stations[]);
+
+/**@fun showTrainSched(TRAINS trainSched[])
+ *	^	This will print out the stations and their ID to the consumer.
+ * 
+ * @param trainSched[] | TRAIN
+ *	^	This array should hold the arrival and departure times for the train.
+ *		They also contain the arrival and departure stations.
+ *
+ * @author NoremacSkich | 2014/4/14
+ *
+ */
+void showTrainSched(STATIONS stations[]);
+
+
+// program main driver
+int main(void); 
+
+// This is a routine to print a prompt and get a user input.  This is so that 
+// when something is sent to the screen, it can be seen before the screen is 
+// cleared.
+void myPause(void);
+
+// This will insert a bunch of new lines to cludge a new screen.
+void clearScreen(void); 
+
+//=========================== FUNCTIONS ===========================
+void showMainMenu(void){
+	
+}
+
+void showStations(STATIONS stations[]){
+	
+}
+
+void showTrainSched(STATIONS stations[]){
+	
+}
+
+void getTrains(string filePath, TRAINS trainSched[]){
+	
+	
+}
+
+void getStations(string filePath, STATIONS stations[]){
+	
+	
+}
+/**@fun myPause(void)
+ *	^	This is a routine to print a prompt and get a user input.  This is 
+ *		so that when something is sent to the screen, it can be seen before 
+ *		the screen is cleared
+ * @author ??? | Sorting Algorithm
+ *
+ */
+void myPause(void)
+{
+	int key;
+	printf("\nEnter the 1 key to continue..");
+	scanf("%i",&key);
+	printf("\n");
+}
+
+/**@fun ClearScreen(void)
+ *	^	This will insert a bunch of new lines to cludge a new screen.
+ *
+ * @note NoremacSkich | 2014/31/3
+ *	^	Originally came from http://goo.gl/ZyMku8
+ */
+void clearScreen(void)
+{
+	cout << string( 50, '\n' );
+}
+
+// ====== main driver ==============================
+int main(void)
+{
+	
+	int main_choice;
+
+	do{
+		// Clear the Screen.
+		clearScreen();
+		
+		// Display the main menu
+		
+		
+		// Get their choice
+		cin >> main_choice;
+		
+		// Check if input failed
+		if(cin.fail()){
+			// It failed, print it out
+			cout << "Input Failed" << endl;
+						// clear the buffer, then continue
+			cin.clear();
+			cin.ignore();
+			myPause();
+
+			continue;
+		}
+		
+		switch(main_choice){
+			
+			
+			case 1: 
+
+				
+				
+				break; // To the main menu
+				
+				
+			case 2:
+				
+				
+				
+				break; // To the main menu
+				
+				
+			case 3:
+				
+									
+				break; // To the main menu
+				
+				
+				
+			case 4:
+				cout << "End Program" << endl;
+				
+				break; // To the main menu
+			
+				
+				
+				
+			default:
+				cout << "oops :)" << endl;
+		}
+	}while(main_choice != 4);
+	
+	// Exit the program gracefully, without errors
+	return 0;
+}
+
+
